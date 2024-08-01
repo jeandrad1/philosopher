99b222c56774310a5f920b3d52be357b5301dde0
jeandrad1 <jesusandradeperez@gmail.com>
2024-08-01 11:57:42 +0200

    now it inits goods

--- src/init_philo.c
+++ src/init_philo.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:24:48 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/08/01 11:28:08 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/08/01 11:55:32 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ static bool init_philo (t_philo *philo, int i)
 {
     philo->id = i;
     philo->left_fork = i;
-    philo->right_fork = i;
-    philo->eat_count = i;
-    philo->last_eat = i;
+    philo->right_fork = i - 1;
+    philo->eat_count = 0;
+    philo->last_eat = 0;
     philo->is_dead = false;
     philo->thread = i;
     return (SUCCESS);
@@ -26,18 +26,20 @@ static bool init_philo (t_philo *philo, int i)
 bool init_all_philo(t_philo *philo, t_table *table)
 {
     int i;
+    int philo_count;
 
     i = 1;
+    philo_count = table->philo_count;
     printf("Init all philo begins\n");
     printf("philo_count: %d\n", table->philo_count);
-    while (i <= table->philo_count)
+    while (i <= philo_count)
     {
-        printf("philo %i initializing\n", i);  
         if(!init_philo(&philo[i], i))
             return (FAILURE);
-        printf("philo %i initialized\n", i);
         i++;
     }
+    philo[1].right_fork = philo_count;
+    philo[philo_count].right_fork = 1;
     printf("Init all philo ends\n");
     return (SUCCESS);
 }
